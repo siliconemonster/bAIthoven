d67968662854661c79d4e194c6b29155f51e2cc0
Aline <alinefrez@netscape.net>
2022-10-16 18:37:03 -0300

    Rebuild from translated string to end

--- src/main_processor.py
+++ src/main_processor.py
@@ -3,6 +3,9 @@ from testing import *
 import os
 import csv
 
+
+# --------------- Parse sonates ---------------
+
 xml_path = os.path.join("pieces")
 
 list_of_every_piece = []
@@ -45,6 +48,8 @@ for file in os.listdir(xml_path):
 
     list_of_every_piece.append(outcome)
 
+    break
+
 flatten_list = []
 string_of_every_piece = ''
 for sonate in list_of_every_piece:
@@ -58,9 +63,54 @@ string_of_every_piece = string_of_every_piece[:-1]
 #print(flatten_list)
 #print(string_of_every_piece)
 
-raw_entry = sorted(set(item for item in string_of_every_piece.split(',')))
-event_into_int = dict((note, number) for number, note in enumerate(raw_entry))
+splitted_string = string_of_every_piece.split(',')
 
-print(event_into_int)
+raw_entry = sorted(set(item for item in splitted_string))
+event_into_int_dict = dict((note, number) for number, note in enumerate(raw_entry))
 
+#print(event_into_int_dict)
+
+translated_string = ''
+for item in splitted_string:
+    translated_string = translated_string + str(event_into_int_dict[item]) + ','
+
+translated_string = translated_string[:-1]
+
+#print(translated_string)
+
+
+# --------------- Build outcome into musical piece ---------------
+
+int_into_event_dict = {value: key for key, value in event_into_int_dict.items()}
+#print(int_into_event_dict)
+
+translated_flat_list = []
+
+for item in translated_string.split(','):
+    parsed_item = int(item)
+    translated_flat_list.append(int_into_event_dict[parsed_item])
+
+#print(translated_flat_list)
+
+no_chord_no_tuplet_output = []
+
+second_positions_3_pos = 'Tempo', 'Tonalidade', 'Formula de Compasso'
+second_positions_6_pos = 'Note', 'Rest', 'Chord'
+
+for index, element in enumerate(translated_flat_list):
+    if any(keyword in element for keyword in second_positions_3_pos):
+        info = [translated_flat_list[index-1], element, translated_flat_list[index+1]]
+        no_chord_no_tuplet_output.append(info)
+    elif 'Tuplet' in element:
+        info = [translated_flat_list[index-1], element, translated_flat_list[index+1],translated_flat_list[index+2],translated_flat_list[index+3],translated_flat_list[index+4]]
+        no_chord_no_tuplet_output.append(info)
+    elif any(keyword in element for keyword in second_positions_6_pos):
+        info = [translated_flat_list[index-1], element, translated_flat_list[index+1],translated_flat_list[index+2],translated_flat_list[index+3],translated_flat_list[index+4]]
+        no_chord_no_tuplet_output.append(info)
+
+#print(no_chord_no_tuplet_output)
+
+outcome = prepare_for_rebuilding(no_chord_no_tuplet_output)
+
+print(outcome)
 
